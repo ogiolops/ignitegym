d86cac9951de7e12c2590e7feda4942523d1d69f
Giovanni lopes <giovanni_lopes@outlook.com>
2023-05-27 18:16:00 -0300

    private routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,44 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+
+import { Exercise } from '@screens/Exercise';
+import { History } from '@screens/History';
+import { Home } from '@screens/Home';
+import { Profile } from '@screens/Profile';
+
+type AppRoutes = {
+  home: undefined,
+  exercise: undefined,
+  profile: undefined,
+  history: undefined,
+} 
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+
+export function AppRoutes() {
+  return(
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+    }} >
+      <Screen 
+        name='home'
+        component={Home}
+      />
+      <Screen 
+        name='history'
+        component={History}
+      />
+      <Screen 
+        name='profile'
+        component={Profile}
+      />
+      <Screen 
+        name='exercise'
+        component={Exercise}
+      />
+    </Navigator>
+  )
+}
