4750a9296b3b35326815d22c01c85834bbe6210b
Giovanni lopes <giovanni_lopes@outlook.com>
2023-05-29 20:37:22 -0300

    adding Header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -68,7 +68,7 @@ export function AppRoutes() {
             />
           )
         }}
-      />
+      /> 
       <Screen 
         name='profile'
         component={Profile}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,6 +19,5 @@ export function Routes(){
         <AppRoutes/>
       </NavigationContainer>
     </Box>
-
   )
 }
