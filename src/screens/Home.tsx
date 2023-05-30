04576748ef2472f8be76a97e5b99f384b5a2f79b
Giovanni lopes <giovanni_lopes@outlook.com>
2023-05-30 10:51:52 -0300

    Style: adding styles to home component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { HStack, VStack, FlatList, Heading, Text } from 'native-base';
 
-import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home(){
 
-  const [groups, setGroups] = useState(['Costa', 'Ombro', 'bícipes', 'perna'])
-  const [groupSelected, setGroupSelected] = useState('ombro')
+  const [groups, setGroups] = useState(['costa', 'Ombro', 'bícipes', 'perna']);
+  const [exercices, setExercices] = useState([' Puxada frontal', 'Remada da morte', 'remada transcendente', 'perna cavalo', 'Panturrilha', 'Agachamento', 'Tricepis']);
+  const [groupSelected, setGroupSelected] = useState('Costa');
 
   return(
     <VStack flex={1} >
@@ -19,9 +21,9 @@ export function Home(){
         renderItem={ ({item}) => (
           <Group 
             name={item} 
-            isActive={groupSelected === item}
+            isActive={groupSelected.toUpperCase() === item.toUpperCase()}
             onPress={() => setGroupSelected(item)}
-        />
+          />
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
@@ -30,16 +32,30 @@ export function Home(){
         maxH={10}
       />
 
-      <HStack justifyContent='space-between' >
-        <Heading color="gray.200" fontSize="md" >
-          Exercícios
-        </Heading>
-        <Text color="gray.200" fontSize="sm" >
-          4
-        </Text>
-      </HStack>
+      <VStack flex={1} px={8} >
+        <HStack justifyContent='space-between' marginBottom={5}>
+          <Heading color="gray.200" fontSize="md" >
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm" >
+            {exercices.length}
+          </Text>
+        </HStack>
+        
+        
+        <FlatList 
+          data={exercices}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <ExerciseCard 
 
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom:20 }}
+        />
 
+      </VStack>
     </VStack>
   );
 }
