45fd9b78f384c3068e317fd9219a651ddfd574c9
Giovanni lopes <giovanni_lopes@outlook.com>
2023-06-02 14:29:53 -0300

    Feat: taking images from the gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -13,6 +15,51 @@ const PHOTO_SIZE = 33;
 
 export function Profile(){
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('http://github.com/ogiolops.png');
+
+  const toast = useToast();
+
+  async function HandleUserPhotorSelect() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+  
+      if (photoSelected.canceled){
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+        console.log(photoInfo.exists)
+
+        if (photoInfo.exists && (photoInfo.size / 1024 / 1024 > 5)) {
+          return toast.show({
+            title: 'Essa imagem é muito grande, escolha uma de até 5MG.',
+            placement: 'top',
+            bgColor: 'red.500'
+          });
+          
+       }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+  
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false);
+    }
+
+  }
+
   return(
     <VStack flex={1} >
       <ScreenHeader 
@@ -32,13 +79,13 @@ export function Profile(){
             />
             :
             <UserPhoto 
-              source={{ uri: 'http://github.com/ogiolops.png' }}
+              source={{ uri: userPhoto }}
               alt='foto do usuário'
               size={PHOTO_SIZE}
             />
             }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={HandleUserPhotorSelect} > 
             <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8} >Alterar foto</Text>
           </TouchableOpacity>
 
